88a7afa4e596fc8511315d9c79fcacf6ddeb9b5e
Rohit Sharma <rs1257933@gmail.com>
2020-02-01 18:05:51 +0530

    database connected

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/Tihor'
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    '''
+    Sno	Name	ph_nm	message	email	date
+    '''
+    Sno = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(80), nullable=False)
+    ph_nm = db.Column(db.String(12), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
 
 @app.route("/")
 def home():
@@ -12,8 +28,17 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/contact")
+@app.route("/contact", methods = ['GET', 'POST'])
 def contact():
+    if(request.method=='POST'):
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contacts(Name=name, ph_nm = phone, message = message, date= datetime.now(),email = email )
+        db.session.add(entry)
+        db.session.commit()
     return render_template('contact.html')
 
 
